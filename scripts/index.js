e2f9a0fd75c38b8d5d455e88bb259a729402ace1
cesarcash <cesarcash5@gmail.com>
2025-02-04 19:13:53 -0600

    avance interfaz de usuario

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,18 @@
-const menu = document.querySelector('#menu');
-console.log("🚀 ~ menu:", menu)
-// const logo = document.querySelector('#headerLogo');
-const sidebar = document.querySelector('#sidebar');
-// const titleMenu = document.querySelector('#title');
+// const menu = document.querySelector('#menu');
+// const sidebar = document.querySelector('#sidebar');
+// const titleHeader = document.querySelector('.aside__title');
+// const imgHeader = document.querySelector('.aside__profile');
 
-menu.addEventListener('click', (e) => {
+// menu.addEventListener('click', (e) => {
     
-    if(e.target.checked) {
-        sidebar.style.width = '4rem';
-        // logo.classList.add('sidebar-headerLogo');
-        // titleMenu.hidden = true;
-    }else{
-        sidebar.style.width = '15.5rem';
-        // logo.classList.remove('sidebar-headerLogo');
-        // titleMenu.hidden = false;
-    }
+//     if(e.target.checked) {
+//         sidebar.style.width = '4rem';
+//         titleHeader.classList.add('aside__title--hidden');
+//         imgHeader.classList.add('aside__title--hidden');
+//     }else{
+//         sidebar.style.width = '15.5rem';
+//         titleHeader.classList.remove('aside__title--hidden');
+//         imgHeader.classList.remove('aside__title--hidden');
+//     }
 
-});
+// });
